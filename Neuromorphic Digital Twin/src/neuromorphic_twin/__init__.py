7c0efeea759d9efbd4b1617e42af1e7ea45a9920
Xtimetraveler-Prime <diego.a.sanchez@student.nmt.edu>
2026-08-03 20:14:50 -0600

    Export FPGA weight storage API

--- Neuromorphic Digital Twin/src/neuromorphic_twin/__init__.py
+++ Neuromorphic Digital Twin/src/neuromorphic_twin/__init__.py
@@ -2,6 +2,25 @@
 
 from .arithmetic import ArithmeticConfig, OverflowMode, round_away_from_zero
 from .core import NeuromorphicCore
+from .fpga_weight_storage import (
+    AXON_ROW_POINTER_BITS,
+    FORMAT_WORD_BITS,
+    FPGA_WEIGHT_STORAGE_SCHEMA,
+    MAX_WEIGHT_FORMATS,
+    SYNAPSE_WORD_BITS,
+    FrozenWeightStorage,
+    PackedSynapseFields,
+    WeightStorageArtifacts,
+    WeightStorageEstimate,
+    estimate_weight_storage,
+    freeze_encoded_synapses,
+    pack_synapse_word,
+    pack_weight_format,
+    read_weight_storage_json,
+    unpack_synapse_word,
+    unpack_weight_format,
+    write_weight_storage_image,
+)
 from .model import NeuronConfig, NeuronState, Spike, Synapse, TickTrace
 from .neuron import step_neuron
 from .weights import (
@@ -12,18 +31,35 @@ from .weights import (
 )
 
 __all__ = [
+    "AXON_ROW_POINTER_BITS",
     "ArithmeticConfig",
-    "OverflowMode",
+    "FORMAT_WORD_BITS",
+    "FPGA_WEIGHT_STORAGE_SCHEMA",
+    "FrozenWeightStorage",
+    "MAX_WEIGHT_FORMATS",
     "NeuronConfig",
     "NeuronState",
     "NeuromorphicCore",
+    "OverflowMode",
+    "PackedSynapseFields",
+    "SYNAPSE_WORD_BITS",
     "Spike",
     "StaticWeightEncoding",
     "Synapse",
     "TickTrace",
     "WeightFormat",
     "WeightSignMode",
+    "WeightStorageArtifacts",
+    "WeightStorageEstimate",
     "encode_static_weight",
+    "estimate_weight_storage",
+    "freeze_encoded_synapses",
+    "pack_synapse_word",
+    "pack_weight_format",
+    "read_weight_storage_json",
     "round_away_from_zero",
     "step_neuron",
+    "unpack_synapse_word",
+    "unpack_weight_format",
+    "write_weight_storage_image",
 ]
