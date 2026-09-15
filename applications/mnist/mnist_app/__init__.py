13a98a54be5aa279ba910a1ca2ea76ded2fa7be1
Xtimetraveler-Prime <diego.a.sanchez@student.nmt.edu>
2026-09-14 19:32:33 -0600

    Expose dual MNIST profiles

--- applications/mnist/mnist_app/__init__.py
+++ applications/mnist/mnist_app/__init__.py
@@ -1,31 +1,39 @@
 """MNIST application layer for the neuromorphic digital twin."""
 
 from .config import (
-    DENSE_SYNAPSES,
-    INPUT_AXONS,
-    INPUT_HEIGHT,
-    INPUT_WIDTH,
+    CROPPED_DENSE,
+    DEFAULT_PROFILE,
+    NATIVE_SPARSE,
     OUTPUT_NEURONS,
     PRESENTATION_TICKS,
+    PROFILES,
+    MnistProfile,
+    get_profile,
 )
 from .encoding import (
     center_crop_20x20,
     count_events,
     encode_binary_spikes,
     encode_event_schedule,
+    normalize_pixels,
+    preprocess_images,
     quantize_spike_levels,
 )
 
 __all__ = [
-    "DENSE_SYNAPSES",
-    "INPUT_AXONS",
-    "INPUT_HEIGHT",
-    "INPUT_WIDTH",
+    "CROPPED_DENSE",
+    "DEFAULT_PROFILE",
+    "NATIVE_SPARSE",
     "OUTPUT_NEURONS",
     "PRESENTATION_TICKS",
+    "PROFILES",
+    "MnistProfile",
     "center_crop_20x20",
     "count_events",
     "encode_binary_spikes",
     "encode_event_schedule",
+    "get_profile",
+    "normalize_pixels",
+    "preprocess_images",
     "quantize_spike_levels",
 ]
